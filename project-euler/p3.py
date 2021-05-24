4ac88b1408978f4f3d7de2f5c1a2ff6d6ca001ca
johnsykim <sababi0314@gmail.com>
2021-05-24 10:50:08 +0900

    Create p3.py

---
+++ project-euler/p3.py
@@ -0,0 +1,29 @@
+# Largest prime factor
+
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
+
+
+def prime_factors(n):       # For complete prime factorization
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+if __name__ == '__main__':
+    print(largest_prime_factor(600851475143))
+    print(prime_factors(600851475143))

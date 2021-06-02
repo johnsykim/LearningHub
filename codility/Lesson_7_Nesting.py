b49d0a577c56048e72260e454c7356353d9e2f03
johnsykim <sababi0314@gmail.com>
2021-06-02 12:50:30 +0900

    Update Lesson_7_Nesting.py

--- codility/Lesson_7_Nesting.py
+++ codility/Lesson_7_Nesting.py
@@ -1,18 +1,25 @@
+# https://datacodingschool.tistory.com/43
+
 def solution(S):
-    nested = {'(': 1, ')': -1}
-    nest = []
-    for s in S:
-        if nested[s] > 0:
-            nest.append(s)
-        if nested[s] < 0:
-            if len(nest) == 0:
-                return 0
-            t = nest.pop()
-            if abs(nested[t]) != abs(nested[s]):
-                return 0
-    if len(nest) != 0:
+    arr = []
+
+    for i in range(len(S)):
+        # print(arr)
+        if S[i] == '(':
+            arr.append(S[i])
+        else:
+            # Instead of adding ')', delete '('
+            if len(arr) != 0 and arr[-1] == '(':
+                del arr[-1]
+            # If the stack is empty
+            else:
+                arr.append(S[i])
+
+    # If S were properly nested, the stack should be empty
+    if len(arr) == 0:
+        return 1
+    else:
         return 0
-    return 1
 
 
 if __name__ == '__main__':

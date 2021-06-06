16f7273a0ab1a421c731f3c1d082764ce2e8039c
johnsykim <sababi0314@gmail.com>
2021-06-06 14:41:10 +0900

    Create fizzbuzz.py

---
+++ kattis/fizzbuzz.py
@@ -0,0 +1,12 @@
+import sys
+
+X, Y, N = map(int, sys.stdin.readline().split())
+for i in range(1, N + 1):
+    if i % X == 0 and i % Y == 0:
+        print("FizzBuzz")
+    elif i % X == 0:
+        print("Fizz")
+    elif i % Y == 0:
+        print("Buzz")
+    else:
+        print(i)
